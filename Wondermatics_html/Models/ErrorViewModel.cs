f8b4c46bdd171ea448020e70373505d043499298
santanon <bakitheone@gmail.com>
2024-07-18 11:50:08 +0700

    Initial commit

---
+++ Wondermatics_html/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Wondermatics_html.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
